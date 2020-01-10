960417ea0073de2f93a7e09e808184727336896c
Pedro Lima e Silva <pedrolimasilva@MacBook-Pro-de-Pedro.local>
2020-01-10 01:41:34 +0000

    Basic build tools

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { src, dest, parallel, watch, series } = require('gulp');
+
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+sass.compiler = require('node-sass');
+
+const htmlmin = require('gulp-htmlmin');
+
+
+function build_sass() {
+  return src('./src/scss/neu.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(dest('./dist/css'));
+}
+
+function watch_sass() {
+  return watch('./src/**/*.scss', build_sass);
+}
+
+function build_html() {
+  return src('./src/demo/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('./dist'));;
+}
+
+function watch_html() {
+  return watch('./src/**/*.html', build_html);
+}
+
+const build_all = parallel(build_sass, build_html);
+const watch_all = parallel(watch_sass, watch_html);
+
+exports.build = build_all;
+exports.watch = watch_all;
+exports.default = series(build_all, watch_all);
